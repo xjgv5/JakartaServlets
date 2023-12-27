24dec5419975a095e3915071f228aade65c0c2d0
Javier Gomez <javier.gov@outlook.es>
2023-12-26 23:13:19 -0600

    Primer servlet

---
+++ src/main/java/com/xjgv/webapp/servlet/HolaMundoServlet.java
@@ -0,0 +1,33 @@
+package com.xjgv.webapp.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hola-mundo")
+public class HolaMundoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.print("<!DOCTYPE html>");
+        out.print("<html>");
+        out.print("     <head>");
+        out.print("         <meta charset=\" UTF-8\">");
+        out.print("         <title>Hola Mundo servlet</title>");
+        out.print("     </head>");
+        out.print("     <body>");
+        out.print("         <h1>Hola mundo servlet</h1>");
+        out.print("     </body>");
+        out.print("</html>");
+        out.close();
+    }
+}
